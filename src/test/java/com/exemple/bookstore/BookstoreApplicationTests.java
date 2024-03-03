2d035178a9f21b16bee741e64f29fa8c2f9f4f99
sarag <saragdl@gmail.com>
2024-03-03 14:39:40 -0300

    Initial commit

---
+++ src/test/java/com/exemple/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemple.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
